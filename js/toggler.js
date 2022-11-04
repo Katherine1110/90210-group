d4cccc089ecafdcd4b423b951183d697dc38674e
Katherine1110 <k.mihalochkina@gmail.com>
2022-11-04 12:41:09 +0200

    updated faq class toggle & styles

--- js/toggler.js
+++ js/toggler.js
@@ -5,16 +5,24 @@ const modal = document.querySelector('[data-modal]');
 const bookingTypes = document.querySelectorAll('.booking__types [data-type]');
 const bookingFormTypes = document.querySelectorAll('.booking__form [data-type]');
 
+const filterTypes = document.querySelectorAll('.products__buttons [data-type]');
+const productsTypes = document.querySelectorAll('.products__types [data-type]');
+
+const faq = document.querySelectorAll('.faq__item');
+const answer = document.querySelectorAll('.faq__answer');
+
+console.log(answer);
 function toggleActiveType() {
   bookingTypes.forEach((type) => {
     type.addEventListener('click', (e) => {
       document.querySelector('.booking__types [data-type].active').classList.remove('active');
-      e.currentTarget.classList.add('active');
+      type.classList.add('active');
 
       bookingFormTypes.forEach((item) => {
         if (e.currentTarget.dataset.type === item.dataset.type) {
           item.classList.remove('hidden');
-        } else if (e.currentTarget.dataset.type !== item.dataset.type) {
+        }
+        if (e.currentTarget.dataset.type !== item.dataset.type) {
           item.classList.add('hidden');
         }
       });
@@ -44,3 +52,48 @@ function toggleModal() {
 }
 
 toggleModal();
+
+function filters() {
+  filterTypes.forEach((type) => {
+    type.addEventListener('click', (e) => {
+      document.querySelector('.products__buttons [data-type].active').classList.remove('active');
+      type.classList.add('active');
+
+      productsTypes.forEach((item) => {
+        if (item.dataset.type === type.dataset.type) {
+          item.classList.remove('hidden');
+        }
+        if (item.dataset.type !== type.dataset.type) {
+          item.classList.add('hidden');
+        }
+      });
+    });
+  });
+}
+
+filters();
+
+// function toggleAnswers() {
+//   faq.forEach((item) => {
+//     item.addEventListener('click', (e) => {
+//       e.currentTarget.classList.toggle('active');
+//       answer.forEach((item) => {
+//         if (item.closest('.active') !== null) {
+//           item.classList.remove('hidden');
+//         } else {
+//           item.classList.add('hidden');
+//         }
+//       });
+//     });
+//   });
+// }
+
+function toggleAnswers() {
+  document.querySelectorAll('.faq__item').forEach((item) => {
+    item.addEventListener('click', (e) => {
+      item.classList.toggle('active');
+    });
+  });
+}
+
+toggleAnswers();
